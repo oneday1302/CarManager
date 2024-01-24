5075d86552ac3d9f8a77db9fc616966a04c1b27c
oneday1302 <kovalchukvladyslav2gmail.com>
2024-01-24 10:12:42 +0100

    added init sql file

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,38 @@
+create sequence maker_seq start with 1 increment by 1;
+
+create table maker
+(
+    id integer not null default nextval('maker_seq') primary key,
+    name text not null unique
+);
+
+create sequence model_seq start with 1 increment by 1;
+
+create table model
+(
+    id integer not null default nextval('model_seq') primary key,
+    maker_id integer not null references maker (id),
+    name text not null unique
+);
+
+create sequence body_type_seq start with 1 increment by 1;
+
+create table body_type
+(
+    id integer not null default nextval('body_type_seq') primary key,
+    name text not null unique
+);
+
+create table car
+(
+    id text not null primary key,
+    model_id integer not null references model (id),
+    production_year smallint not null
+);
+
+create table car_body_type
+(
+    car_id text not null references car (id) on delete cascade on update cascade,
+    body_type_id integer not null references body_type (id),
+    constraint car_body_type_pkey primary key (car_id, body_type_id)
+);
