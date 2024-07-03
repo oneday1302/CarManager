b817498ae43bfc66e4d7396b02107a3e706cbf7a
oneday1302 <kovalchukvladyslav2gmail.com>
2024-07-03 17:13:18 +0200

    added custom valid annotation

--- src/main/java/ua/foxminded/javaspring/carmanager/validation/annotation/NotExist.java
+++ src/main/java/ua/foxminded/javaspring/carmanager/validation/annotation/NotExist.java
@@ -6,10 +6,10 @@ import jakarta.validation.Payload;
 import java.lang.annotation.*;
 
 @Documented
-@Constraint(validatedBy = ZeroValidator.class)
+@Constraint(validatedBy = NotExistValidator.class)
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface Zero {
+public @interface NotExist {
 
     String message() default "";
 
